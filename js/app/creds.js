8314f9c86dc987114eea18c97b4a6c9df84bdae3
Nicholas Reed <nicholas.a.reed@gmail.com>
2012-10-02 17:07:28 -0700

    .

--- js/app/creds.js
+++
@@ -1,6 +0,0 @@
-
-var tmp_credentials = {
-	base_api_url: "https://cryptic-everglades-7993.herokuapp.com",
-	fullcontact_api_key: "cc88e1354d47cd34",
-	filepicker_api_key: "ANmCrulqRQcympGJ9kxmEz"
-};

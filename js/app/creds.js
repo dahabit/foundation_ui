c2fdedf6415fbb74a136ae3190fdc84464b2f94a
Nicholas Reed <nicholas.a.reed@gmail.com>
2012-11-07 23:26:31 -0800

    allowing creds

---
+++ js/app/creds.js
@@ -0,0 +1,10 @@
+
+var tmp_credentials = {
+	//base_api_url: "https://cryptic-everglades-7993.herokuapp.com",
+	base_api_url: "http://getemailbox.com:8080",
+	ui_app_key: "com.ui",
+	fullcontact_api_key: "",
+	filepicker_api_key: "ANmCrulqRQcympGJ9kxmEz",
+
+	s3_bucket: "https://s3-us-west-1.amazonaws.com/emailbox1/"
+};

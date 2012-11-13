3d30a52c0db1601c1e963ff6cc4e9c2593119329
Nicholas Reed <nicholas.a.reed@gmail.com>
2012-11-13 14:24:25 -0800

    .

--- js/app/creds.js
+++ js/app/creds.js
@@ -1,8 +1,8 @@
 
 var tmp_credentials = {
 	//base_api_url: "https://cryptic-everglades-7993.herokuapp.com",
-	base_api_url: "http://getemailbox.com:8080",
-	ui_app_key: "com.ui",
+	base_api_url: "http://getemailbox.com:80",
+	ui_app_key: "pkg.native.ui",
 	fullcontact_api_key: "",
 	filepicker_api_key: "ANmCrulqRQcympGJ9kxmEz",
 
